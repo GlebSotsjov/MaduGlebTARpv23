1e0292dbffd2353c4458d6394ec4875dc6406cc2
GlebSotsjov <153904865+GlebSotsjov@users.noreply.github.com>
2024-09-11 11:40:17 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,16 +1,36 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace MaduGlebTARpv23
 {
     internal class Program
     {
-        static void Main(string[] args) 
+        static void Main(string[] args)
         {
+
+            
+            Console.SetBufferSize(80, 25);
+
+            HorizontalLine upLine = new HorizontalLine(0, 78, 0, '+');
+            HorizontalLine downLine = new HorizontalLine(0, 78, 24, '+');
+
+            VerticalLine leftLine = new VerticalLine(0, 24, 0, '+');
+            VerticalLine rightLine = new VerticalLine(0, 24, 78, '+');
+
+            upLine.Drow();   
+            downLine.Drow(); 
+            leftLine.Drow(); 
+            rightLine.Drow(); 
+
+            Point p = new Point(4, 5, '*');
+            Snake snake = new Snake(p, 4, Direction.RIGHT);
+            snake.Drow(); 
+
             Console.OutputEncoding = System.Text.Encoding.UTF8;
             Point p1 = new Point(1,3, '*');
             p1.Draw();
@@ -22,6 +42,7 @@ namespace MaduGlebTARpv23
             Line.Drow();
 
             Console.ReadLine();
+
         }
     }
 }
