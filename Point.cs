f4a5bbc97c063aeaaa7f8cac534b0ebf1eb3ae85
GlebSotsjov <153904865+GlebSotsjov@users.noreply.github.com>
2024-09-11 09:52:07 +0300

    Update Point.cs

--- Point.cs
+++ Point.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,25 +8,57 @@ namespace MaduGlebTARpv23
 {
     internal class Point
     {
-        public int x; 
+        public int x;
         public int y;
         public char sym;
 
-        public Point() 
+        public Point()
         {
         }
 
-        public Point(int _x, int _y, char _sym) 
+        public Point(int _x, int _y, char _sym)
         {
             x = _x;
             y = _y;
             sym = _sym;
         }
 
+        public Point(Point p)
+        {
+            x = p.x;
+            y = p.y;
+            sym = p.sym;
+        }
+
+        public void Move(int offset, Direction direction)
+        {
+            if (direction == Direction.RIGHT)
+            {
+                x = x + offset;
+            }
+            else if (direction == Direction.LEFT)
+            {
+                x = x - offset;
+            }
+            else if (direction == Direction.UP)
+            {
+                y = y + offset;
+            }
+            else if (direction == Direction.DOWN)
+            {
+                y = y - offset;
+            }
+        }
+
         public void Draw()
         {
             Console.SetCursorPosition(x, y);
-            Console.WriteLine(sym);
+            Console.Write(sym);
+        }
+
+        public override string ToString()
+        {
+            return x + ", " + y + ", " + sym;
         }
     }
 }
