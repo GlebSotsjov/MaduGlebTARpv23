3553a18d288c6f1a88fcb534f4b05d330deded10
GlebSotsjov <153904865+GlebSotsjov@users.noreply.github.com>
2024-09-11 09:48:54 +0300

    Create Direction.cs

---
+++ Direction.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaduGlebTARpv23
+{
+    enum Direction
+    {
+        LEFT,
+        RIGHT,
+        UP,
+        DOWN
+    }
+}
