0eb18fdd0213a7c7b59d06f16994d1daad038527
GlebSotsjov <153904865+GlebSotsjov@users.noreply.github.com>
2024-09-16 15:41:54 +0300

    Update Walls.cs

--- Walls.cs
+++ Walls.cs
@@ -1,38 +1,28 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MaduGlebTARpv23
 {
     internal class Walls
     {
-        // Seinaelementide loend, mis hoiab mänguvälja seinte kujundeid
         List<Figure> wallList;
+        private Pixel wallPixel = new Pixel('+', ConsoleColor.Red); // Красные стены
 
-        // Konstruktor, mis loob mänguvälja seinad
         public Walls(int mapWidth, int mapHeight)
         {
             wallList = new List<Figure>();
 
-            // Ülemine horisontaalne sein
-            HorizontalLine upLine = new HorizontalLine(0, mapWidth - 2, 0, '+');
-            // Alumine horisontaalne sein
-            HorizontalLine downLine = new HorizontalLine(0, mapWidth - 2, mapHeight - 1, '+');
-            // Vasak vertikaalne sein
-            VerticalLine leftLine = new VerticalLine(0, mapHeight - 1, 0, '+');
-            // Parem vertikaalne sein
-            VerticalLine rightLine = new VerticalLine(0, mapHeight - 1, mapWidth - 2, '+');
+            HorizontalLine upLine = new HorizontalLine(0, mapWidth - 2, 0, wallPixel.Symbol);
+            HorizontalLine downLine = new HorizontalLine(0, mapWidth - 2, mapHeight - 1, wallPixel.Symbol);
+            VerticalLine leftLine = new VerticalLine(0, mapHeight - 1, 0, wallPixel.Symbol);
+            VerticalLine rightLine = new VerticalLine(0, mapHeight - 1, mapWidth - 2, wallPixel.Symbol);
 
-            // Lisame kõik seinad loendisse
             wallList.Add(upLine);
             wallList.Add(downLine);
             wallList.Add(leftLine);
             wallList.Add(rightLine);
         }
 
-        // Kontrollib, kas kujund (näiteks madu) põrkab vastu seina
         internal bool IsHit(Figure figure)
         {
             foreach (var wall in wallList)
@@ -45,7 +35,6 @@ namespace MaduGlebTARpv23
             return false;
         }
 
-        // Joonistab kõik seinad ekraanile
         public void Draw()
         {
             foreach (var wall in wallList)
